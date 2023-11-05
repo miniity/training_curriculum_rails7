010fc32f1518bdca0d02d2ef446f8a2e517d65ea
miniity <miniitiy.18@gmail.com>
2023-11-05 20:18:47 +0900

    修正_1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,9 +34,8 @@ class Calendarscontroller < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-#     days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-#     @week_days.push(days)
-      @week_days.push(days.transform_keys { |key| key.to_sym })
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans }
+      @week_days.push(days)
     end
 
   end
