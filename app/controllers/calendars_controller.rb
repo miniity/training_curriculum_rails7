15373e76062c70a8239d477300f68f77b14c650e
miniity <miniitiy.18@gmail.com>
2023-11-06 21:30:02 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,4 @@
-class Calendarscontroller < ApplicationController
+class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
@@ -20,6 +20,7 @@ class Calendarscontroller < ApplicationController
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @weekdays = wdays
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
